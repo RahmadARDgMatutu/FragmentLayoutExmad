61a80bf19ad33051bf7823662888771c8b7105c1
Rahmad <ariefrahmanpatta@gmail.com>
2022-11-20 21:39:36 +0800

    Initial commit

---
+++ app/src/main/java/com/rahmad_f55121042/fragmentlayoutexmad/ViewAdapter.java
@@ -0,0 +1,7 @@
+package com.rahmad_f55121042.fragmentlayoutexmad;
+
+import androidx.fragment.app.Fragment;
+
+public interface ViewAdapter {
+    void AddFragment(Fragment fragment, String Title);
+}
